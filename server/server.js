0d25e940c5e632d852b15fa519f7fab6e67933b8
Youichi <46785068+youichiuehara14@users.noreply.github.com>
2025-05-28 11:59:19 +0800

    fix(server.js): added import path

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import todoRoute from './routes/todoRoute.js';
+import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 5000;
 // app.use(cors());
